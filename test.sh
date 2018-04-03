4e3db71b96ea67bc5e95e855ab37a6e7c81eeb1e
Jamie Jennings <jjennings@us.ibm.com>
2018-04-03 12:09:32 -0400

    Test script now takes argument that is which python to test against

--- test.sh
+++ test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+python_bin=$1
+default="python"
+if [ -z "$python_bin" ]; then
+    echo "Using default python executable: $default"
+    echo "To use a different python, use: `basename $0` python_executable_name" 
+    python_bin=$default
+fi
 cd /tmp
-echo "Testing in directory `pwd` with `python --version 2>&1`"
-python -c <<EOF '
+echo ""
+echo "Testing in directory `pwd` with `$python_bin --version 2>&1`"
+$python_bin -c <<EOF '
 from __future__ import print_function
 import rosie, json
 e = rosie.engine()
